b2aba77f73e0223d3a2af97ac276c13d0497eb9b
Josh Webb <joshwebb42@gmail.com>
2019-07-16 15:17:11 -0500

    firster commit

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+
+            IEnumerable<string> LFruits = from fruit in fruits
+                where fruit.StartsWith("L")
+                select fruit;
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(x => x % 4 == 0 || x % 6 == 0);
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre"
+            };
+
+            List<string> descend = names.OrderByDescending(n => n).ToList();
+
+            // Build a collection of these numbers sorted in ascending order
+            
+            var ascend = numbers.OrderBy(n => n);
+
+            var howManyNumbers = numbers.Count();
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            var howMuchMoneyHaveWeMade = purchases.Sum();
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            var mostExpensive = prices.Max();
+
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
+
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<int> wheresSquaredo = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+
+            var perfectSquare = wheresSquaredo.TakeWhile(num => {
+                int number = Convert.ToInt32(Math.Sqrt(num));
+                return number * number != num;
+            });
+
+            var alternate = wheresSquaredo.TakeWhile(num => Math.Sqrt(num) % 1 != 0);
+
+            foreach(int num in alternate){
+                Console.WriteLine(num);
+            }
+
+            
+
+                        
+            
+                }
+        
+
+
+        }
+    }
+
